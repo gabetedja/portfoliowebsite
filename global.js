0148de54237df978fe757e1af2e11717436c1c4d
Gabriel Tedja <gabetedja3@gmail.com>
2025-10-16 22:21:57 -0700

    Ok second times the charm added aesthetic functionality back to automatic nav bar

--- global.js
+++ global.js
@@ -4,6 +4,14 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/portfoliowebsite/";         // GitHub Pages repo name
+
+  if (!url.startsWith('http')) {
+  url = BASE_PATH + url;
+}
+
 let navLinks = $$("nav a");
 
 let currentLink = navLinks.find(
@@ -33,16 +41,13 @@ for (let p of pages) {
     a.href = url;
     a.textContent = title;
     nav.append(a);
+
+    if (a.host !== location.host) a.target = "_blank";
+
     if (a.host === location.host && a.pathname === location.pathname) {
         a.classList.add('current');
     }
 }
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/portfoliowebsite/";         // GitHub Pages repo name
 
-  if (!url.startsWith('http')) {
-  url = BASE_PATH + url;
-}
 
