e5c5eb27667489bf428d94669dc0d2ae04b5f283
Gabriel Tedja <gabetedja3@gmail.com>
2025-10-24 20:35:38 -0700

    Fiddled around with github apistuff

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,10 @@
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const container = document.querySelector('.projects');
+const title = document.querySelector('.projects-title');
+if (title) {
+    title.textContent = `${projects.length} Projects`;
+}
 console.log('projects loaded?', Array.isArray(projects), 'length:', projects?.length, projects?.[0]);
 
 projects.forEach(p => renderProjects(p, container, 'h2'));
